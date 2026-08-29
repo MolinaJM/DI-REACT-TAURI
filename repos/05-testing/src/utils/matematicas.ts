359432f5132c08c0e4fc77a4af26c4cf6d2b662a
MolinaJM <jmmolina@ieshlanz.es>
2026-08-29 20:27:21 +0200

    Inicio

--- repos/05-testing/src/utils/matematicas.ts
+++
@@ -1,7 +0,0 @@
-export function sumar(a: number, b: number): number {
-    return a + b;
-}
-
-export function filtrarPares(numeros: number[]): number[] {
-    return numeros.filter(n => n % 2 === 0);
-}
